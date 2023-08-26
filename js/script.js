5fdd3bcd22bd5837aac1351917cdfd313b0a00b3
Faithnwogu <dfaytech@gmail.com>
2023-08-26 10:59:32 +0100

    complete code for photoworld website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const mobileIcon = document.querySelector('#mobileicon');
+const mobile = document.querySelector('#mobile');
+const exit = document.querySelector('#exit');
+
+
+mobileIcon.addEventListener('click', ()=>{
+     mobile.classList.toggle('toggle')
+    
+      
+      
+})
